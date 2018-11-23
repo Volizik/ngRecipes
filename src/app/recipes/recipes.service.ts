6bf482a1db4ca3cc68ebd13921446cfc20532a2c
Volizik <kizilov.yura@gmail.com>
2018-11-23 13:42:07 +0300

    first

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,48 @@
+import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
+import {EventEmitter, Injectable} from "@angular/core";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipesService {
+
+    recipeSelected = new EventEmitter<Recipe>();
+    private recipes: Recipe[] = [
+        new Recipe(
+            'Pancakes',
+            'French pancakes with meat',
+            'http://www.thepersianpot.com/wp-content/uploads/IMG-20150307-WA00111.jpg',
+            [
+                new Ingredient('Meat', 3),
+                new Ingredient('Meal', 2),
+                new Ingredient('Water', 1),
+                new Ingredient('Eggs', 2),
+            ]
+        ),
+        new Recipe(
+            'Burger',
+            'American burger',
+            'https://www.seriouseats.com/recipes/images/2015/07/20150702-sous-vide-hamburger-anova-primary-1500x1125.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('Bun', 1),
+                new Ingredient('Cheese', 1),
+                new Ingredient('Sauce', 1),
+            ]
+        )
+    ];
+
+    constructor(private slService: ShoppingListService) {}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
+    }
+
+    addIngredientToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients)
+    }
+}
---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,14 @@
+import {Directive, HostBinding, HostListener} from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective {
+
+    @HostBinding('class.open') isOpened = false;
+
+    @HostListener('click') toggleDropdown() {
+        this.isOpened = !this.isOpened;
+    }
+
+}
